1b915f5839f42705df67c2c4b6e54f72adfa3119
SargeMahoney <50545858+SargeMahoney@users.noreply.github.com>
2021-02-23 10:31:21 +0100

    Update room query list handler

--- SoundCore.Application/Features/Rooms/Queries/GetRoomsList/GetRoomListQueryHandler.cs
+++ SoundCore.Application/Features/Rooms/Queries/GetRoomsList/GetRoomListQueryHandler.cs
@@ -1,6 +1,8 @@
-﻿using MediatR;
-using System;
+﻿using AutoMapper;
+using MediatR;
+using SoundCore.Application.Contracts.Persistence;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -8,9 +10,18 @@ namespace SoundCore.Application.Features.Rooms.Queries.GetRoomsList
 {
     public class GetRoomListQueryHandler : IRequestHandler<GetRoomListQuery, List<RoomListVm>>
     {
-        public Task<List<RoomListVm>> Handle(GetRoomListQuery request, CancellationToken cancellationToken)
+        private readonly IMapper _mapper;
+        private readonly IRoomsRepository _roomRepository;
+
+        public GetRoomListQueryHandler(IMapper mapper, IRoomsRepository roomRepository)
         {
-            throw new NotImplementedException();
+            this._mapper = mapper;
+            this._roomRepository = roomRepository;
+        }
+        public async Task<List<RoomListVm>> Handle(GetRoomListQuery request, CancellationToken cancellationToken)
+        {
+            var allCategories = (await this._roomRepository.ListAllAsync()).OrderBy(x => x.Name);
+            return this._mapper.Map<List<RoomListVm>>(allCategories);
         }
     }
 }
