0d742cf38d32a4e99b62815af426709443f2bef7
SargeMahoney <50545858+SargeMahoney@users.noreply.github.com>
2021-02-25 16:05:44 +0100

    Created Blazor Server App
    Main UI , configured for start as services on Windows Envroinment

--- SoundCore.Persistence/SqlServer/SqlServerPersistenceServicesRegistration.cs
+++ SoundCore.Persistence/SqlServer/SqlServerPersistenceServicesRegistration.cs
@@ -1,15 +1,22 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using SoundCore.Application.Configurations.DatabaseSettings;
+using SoundCore.Application.Contracts.Persistence;
+using SoundCore.Persistence.SqlServer.Repositories;
 
 namespace SoundCore.Persistence.SqlServer
 {
     public static class SqlServerPersistenceServicesRegistration
     {
 
-        public static IServiceCollection AddSqlServerPersistenceServicesRegistraiton(this IServiceCollection services, IConfiguration configuration)
+        public static IServiceCollection AddSqlServerPersistenceServicesRegistration(this IServiceCollection services, IConfiguration configuration)
         {
             services.Configure<DatabaseSetting>(configuration.GetSection("DatabaseConfiguration"));
+            services.AddSingleton<IDatabaseSetting>(sp => sp.GetRequiredService<IOptions<DatabaseSetting>>().Value);
+
+            services.AddTransient<IRoomsRepository, RoomsRepository>();
+            services.AddScoped<IRoomsRepository, RoomsRepository>();
 
             return services;
          
---
+++ SoundCore.Server/Controllers/api/RoomsController.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SoundCore.Application.Features.Rooms.Queries.GetRoomsList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SoundCore.Server.Controllers.api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RoomsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public RoomsController(IMediator mediator)
+        {
+            this._mediator = mediator;
+        }
+
+        [HttpGet("all", Name = "GetAllCategories")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<ActionResult<List<RoomListVm>>> GetAllRooms()
+        {
+            var dtos = await _mediator.Send(new GetRoomListQuery());
+            return Ok(dtos);
+        }
+    }
+}
